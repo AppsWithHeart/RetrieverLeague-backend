6bd0ae2256f0c9317ae65b0e41d2debdaba4746d
Uros Zivaljevic <uroszivaljevic19@gmail.com>
2017-08-08 19:59:35 -0700

    fix get dog

--- routes/dogs.js
+++ routes/dogs.js
@@ -30,7 +30,7 @@ router.get('/', function (req, res) {
         })
 });
 
-router.get('/:dogId', function (req, res) {
+router.get('/:dogId/tasks/:contestId', function (req, res) {
     models.Dog.findById(req.params.dogId, {
         include: [
             {
