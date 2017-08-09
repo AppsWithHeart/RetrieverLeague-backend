b92b53f606c8678a0fa1db61af24c77217d7f9d4
Uros Zivaljevic <uroszivaljevic19@gmail.com>
2017-08-08 20:44:47 -0700

    fix get tasks

--- models/Task.js
+++ models/Task.js
@@ -12,6 +12,7 @@ module.exports = function (sequelize, DataTypes) {
     Task.associate = function (models) {
         Task.belongsTo(models.Contest, {foreignKey: 'contestId'});
         Task.belongsToMany(models.Dog, {through: models.DogTask, foreignKey: 'taskId', as: 'dogs'});
+        Task.hasMany(models.DogTask, {foreignKey: 'taskId', as: 'dogTasks'});
     };
 
     return Task;
